0ae10acbdb3c40841a585bffa8c49f7267bbc7c0
SeongJin Jeong <jsj0718tjdwl@naver.com>
2020-03-03 23:54:44 +0900

    Added styles to footer

--- src/pageSrc/publicSrc/footer.jsx
+++ src/pageSrc/publicSrc/footer.jsx
@@ -1,13 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-class footer extends Component{
-    render(){
-        return(
-            <div className="text-center sticky-bottom">
-                <p>All Copyrights Deserved from Son...haha</p>
-            </div>
-        )
-    }
-};
+class footer extends Component {
+  render() {
+    return (
+      <div
+        className="text-center sticky-bottom border-top border-dark pt-1"
+        style={footerStyle}
+      >
+        <p>All Copyrights Deserved from Son...haha</p>
+      </div>
+    );
+  }
+}
+
+const footerStyle = {};
 
 export default footer;
